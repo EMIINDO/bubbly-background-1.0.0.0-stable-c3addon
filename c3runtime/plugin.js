6bc200c2225eb2ad0c265e8391586eedd88db6e1
EMI-INDO <78555833+EMI-INDO@users.noreply.github.com>
2025-02-07 04:24:08 +0700

    Force update commit

---
+++ c3runtime/plugin.js
@@ -0,0 +1,12 @@
+"use strict";
+
+{
+	const C3 = globalThis.C3;
+    C3.Plugins.StraniAnelli_BubblyBackground = class BubblyBackgroundPlugin extends globalThis.ISDKPluginBase
+	{
+		constructor()
+		{
+			super();
+		}
+	};
+}
---
+++ c3runtime/type.js
@@ -0,0 +1,14 @@
+"use strict";
+
+{
+	const C3 = globalThis.C3;
+    C3.Plugins.StraniAnelli_BubblyBackground.Type = class BubblyBackgroundType extends globalThis.ISDKObjectTypeBase
+	{
+		constructor()
+		{
+			super();
+		}
+		_onCreate() {
+		}
+	};
+}
